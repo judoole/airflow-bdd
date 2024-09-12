fab74e4911852b99d7cecbb053cec82475c5c827
Ole Christian Langfjæran <judoole@gmail.com>
2024-09-12 15:19:22 -0700

    Add some narratives

--- tests/test_bdd.py
+++ tests/test_bdd.py
@@ -20,12 +20,20 @@ def test_with_context_manager():
 
 @airflow_bdd()
 def test_given_a_dag(bdd: Scenario):
+    """As a developer
+    I want to add a random DAG to the context
+    So that I can use it for adding and testing tasks
+    """
     bdd.given(a_dag())
     bdd.then(it_should(instance_of(DAG)))
 
 
 @airflow_bdd()
 def test_given_the_dag(bdd: Scenario):
+    """As a developer
+    I want to create a DAG from scratch
+    So that I can create tests using specific DAG configurations
+    """
     bdd.given(dag(DAG(
         dag_id="my_dag",
         schedule_interval=None,
@@ -37,6 +45,10 @@ def test_given_the_dag(bdd: Scenario):
 
 @airflow_bdd()
 def test_given_a_tasks_on_a_dag(bdd: Scenario):
+    """As a developer
+    I want to add tasks to a DAG
+    So that I can test the tasks
+    """
     bdd.given(a_dag())
     bdd.and_given(a_task(EmptyOperator(task_id="task_1")))
     bdd.and_given(a_task(EmptyOperator(task_id="task_2")))
@@ -46,6 +58,10 @@ def test_given_a_tasks_on_a_dag(bdd: Scenario):
 
 @airflow_bdd()
 def test_rendering_of_a_task(bdd: Scenario):
+    """As a developer
+    I want to render a task
+    So that I can assert the rendered template
+    """
     bdd.given(a_dag())
     bdd.and_given(execution_date("2020-01-01"))
     bdd.and_given(a_task(
@@ -64,6 +80,10 @@ def test_rendering_of_a_task(bdd: Scenario):
 ])
 @airflow_bdd()
 def test_with_pytest_params(bdd: Scenario, task_id):
+    """As a developer
+    I want to be able to use pytest.mark.parametrize
+    So that I can run the same test with different parameters
+    """
     bdd.given(a_dag())
     bdd.and_given(a_task(EmptyOperator(task_id=task_id)))
     bdd.then(it_should(has_property("task_id", equal_to(task_id))))
